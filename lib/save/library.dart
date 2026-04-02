dd87fd3d56273d9ea2404af34c1e4061fc2e0d10
Thurler <guithurler90@gmail.com>
2026-04-02 19:23:08 -0300

    Added library data save structs

---
+++ lib/save/library.dart
@@ -0,0 +1,57 @@
+import 'dart:typed_data';
+
+import 'package:tfields/extensions.dart';
+
+/// The library levels data for a character's data
+class LibraryData {
+  static const int libraryCap = 2147483647; // Overflows past this
+
+  /// How many upgrades were spent on the HP stat
+  final int hp;
+
+  /// How many upgrades were spent on the ATK stat
+  final int atk;
+
+  /// How many upgrades were spent on the DEF stat
+  final int def;
+
+  /// How many upgrades were spent on the MAG stat
+  final int mag;
+
+  /// How many upgrades were spent on the MND stat
+  final int mnd;
+
+  /// How many upgrades were spent on the SPD stat
+  final int spd;
+
+  /// Get the data according to the stat index
+  int getData(int index) => <int>[hp, atk, def, mag, mnd, spd][index];
+
+  LibraryData({
+    required this.hp,
+    required this.atk,
+    required this.def,
+    required this.mag,
+    required this.mnd,
+    required this.spd,
+  });
+
+  /// Initialize the library data from the provided [bytes]
+  LibraryData.fromBytes(Endian endianness, Uint8List bytes, int offset) :
+    hp = bytes.getU32(endianness, offset: offset),
+    atk = bytes.getU32(endianness, offset: offset + 4),
+    def = bytes.getU32(endianness, offset: offset + 8),
+    mag = bytes.getU32(endianness, offset: offset + 12),
+    mnd = bytes.getU32(endianness, offset: offset + 16),
+    spd = bytes.getU32(endianness, offset: offset + 20);
+
+  /// Return the 4-byte bytes representation of the library stats
+  Iterable<int> toBytes(Endian endianness) => <int>[
+    ...hp.toU32(endianness),
+    ...atk.toU32(endianness),
+    ...def.toU32(endianness),
+    ...mag.toU32(endianness),
+    ...mnd.toU32(endianness),
+    ...spd.toU32(endianness),
+  ];
+}
