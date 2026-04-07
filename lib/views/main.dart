c6f21c0bf14f72d5482791f4f79669d65c67ae72
Thurler <guithurler90@gmail.com>
2026-04-07 18:17:18 -0300

    Integrated save file loading into main view

--- lib/views/main.dart
+++ lib/views/main.dart
@@ -1,16 +1,16 @@
 import 'dart:async';
 import 'dart:io';
-//import 'dart:typed_data';
-//import 'package:file_picker/file_picker.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:tfields/extensions.dart';
 import 'package:tfields/logging.dart';
 import 'package:tfields/settings.dart';
 import 'package:tfields/theme.dart';
 import 'package:tfields/update_check.dart';
 import 'package:tfields/widgets.dart';
 import 'package:thlaby3_save_editor/mixins/navigate.dart';
-//import 'package:thlaby2_save_editor/save.dart';
+import 'package:thlaby3_save_editor/save.dart';
 import 'package:thlaby3_save_editor/widgets/exception.dart';
 
 class MainUpdateCheck extends TUpdateCheck {
@@ -19,6 +19,7 @@ class MainUpdateCheck extends TUpdateCheck {
       'https://api.github.com/repos/thurler/thlaby3-save-editor/releases/latest';
 }
 
+/// The main menu that prompts the user to select a save file directory
 class MainWidget extends StatefulWidget {
   static const String version = '0.0.0';
 
@@ -30,98 +31,86 @@ class MainWidget extends StatefulWidget {
 
 class MainState extends State<MainWidget>
     with
-        //SaveEditor,
-        //SaveLoader,
+        SaveEditor,
+        SaveLoader,
         TLoggable,
         TSettingsJsonReader<TCommonSettings>,
         TCommonSettingsDeserializer,
         TUpdateChecker<MainUpdateCheck>,
         TDialogDisplayer<MainWidget>,
         Navigatable<MainWidget> {
+  // Simply rebuild when the update check is done
   @override
   void updateCheckCallback() => setState(() {});
 
   @override
   Future<void> navigateToSettings() async {
     await super.navigateToSettings();
+    // Make sure to reload settings after navigating
     setState(() {
       readSettings();
     });
+    // And once again check for updates, in case the option was just enabled
     if (settings.checkUpdates) {
       unawaited(checkForUpdates(MainWidget.version));
     }
   }
 
   @override
-  MainUpdateCheck updateChecker = MainUpdateCheck();
+  final MainUpdateCheck updateChecker = MainUpdateCheck();
 
   Future<void> _handleFileSystemException(FileSystemException e) {
     return showException(
-      'An error occured when reading the file!',
-      logMessage: 'FileSystem Exception when loading file: ${e.message}',
-      body: 'Make sure your user has permission to read the file you chose.',
-    );
-  }
-
-  Future<void> _handleFormatException(String logMessage) {
-    return showException(
-      'The selected files are invalid!',
-      logMessage: logMessage,
-      body: 'Make sure you chose a valid save file directory. It should '
+      'An error occured when reading the files!',
+      logMessage: 'FileSystem Exception when loading files: ${e.message}',
+      body: 'Make sure your user has permission to read the directory you '
+          'chose, and that it is a valid save file directory.\n\nIt should '
           'contain files like "C001.dat" and "0101_OD.txt" inside the '
           '"SaveData" folder in your local game files.',
     );
   }
 
+  Future<void> _handleFormatException(SaveException e) {
+    return showException(
+      'The selected files are invalid!',
+      logMessage: e.logMessage,
+      body: 'Make sure you chose a valid save file directory. It should '
+          'contain files like "C001.dat" and "0101_OD.txt" inside the '
+          '"SaveData" folder in your local game files.\n\n${e.userMessage}',
+    );
+  }
+
   Future<void> _loadSaveFile() async {
     await log(TLogLevel.debug, 'Load Save File called');
-    //FilePickerResult? result = await FilePicker.platform.pickFiles(
-    //  type: FileType.custom,
-    //  allowedExtensions: <String>['dat'],
-    //);
-    //if (result == null) {
-    //  await log(TLogLevel.debug, 'No file selected');
-    //  return;
-    //}
+    String? result = await FilePicker.platform.getDirectoryPath(
+      dialogTitle: 'Select the directory containing the save file',
+      lockParentWindow: true,
+    );
+    if (result == null) {
+      await log(TLogLevel.debug, 'No directory selected');
+      return;
+    }
     try {
       await log(TLogLevel.info, 'Loading save file');
-      //String path = result.files.first.path ?? '';
-      //String name = path.split('/').last.split(r'\').last;
-      //await log(TLogLevel.debug, 'File selected: $name');
-      //File rawFile = File(path);
-      //Uint8List bytes = await rawFile.readAsBytes();
-      //saveFile = SaveFile.fromSteamBytes(bytes);
-      await logFlush();
+      // Make sure we remove trailing slashes
+      if (result.last == '/' || result.last == r'\') {
+        result = result.substring(0, result.length - 1);
+      }
+      await log(TLogLevel.debug, 'Directory selected: $result');
+      // Load the save file and parse its data
+      saveFile = await SaveFile.fromSaveDir(result);
       await log(TLogLevel.info, 'Save file loaded successfully');
     } on FileSystemException catch (e) {
       await _handleFileSystemException(e);
       return;
-      //} on FileSizeException {
-      //  await _handleFormatException(
-      //    'File loaded does not have correct number of bytes',
-      //  );
-      //  return;
-      //} on InvalidHeaderException {
-      //  await _handleFormatException(
-      //    'File loaded does not have the correct header bytes',
-      //  );
-      //  return;
-    } on Exception catch (e, s) {
+    } on SaveException catch (e) {
+      await _handleFormatException(e);
+      return;
+    } catch (e, s) {
       await showUnexpectedException(e, s, body: ExceptionWidget.dialogBody);
       return;
     }
-    //if (saveFile.loadedWithErrors) {
-    //  await showWarning(
-    //    'The loaded save file had errors',
-    //    body: 'Some of the data that was read seemed to contain invalid '
-    //        'values. Please check the "applicationlog.txt" file for more '
-    //        'information.\n\nIf you are sure the uploaded file was not '
-    //        'tampered with, please report this as an issue at the link '
-    //        'below, including your save file and the "applicationlog.txt" '
-    //        'file:\nhttps://github.com/Thurler/thlaby3-save-editor/issues',
-    //  );
-    //  return;
-    //}
+    // If successful, navigate to the main menu
     return navigateToMainMenu();
   }
 
